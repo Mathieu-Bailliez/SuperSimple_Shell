e38430f9399cce86153a82a4c8c540b7047ba0bb
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-20 12:12:18 +0200

    mod: use the official example on holberton intranet

--- pid.c
+++ pid.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-#include <unistd.h> /* pour getpid() et getppid() */
+#include <unistd.h>
 
 /**
-  * main - affiche le PID et le PPID du processus actuel
-  *
-  * Return: Always 0.
-  */
-
+ * main - PID
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	pid_t my_pid;
-	pid_t my_ppid;
 
-	my_pid = getpid(); /* récupére mon propre PID */
-	my_ppid = getppid(); /* récupére le PID de mon parent */
-
-	printf("Mon PID : %u\n", my_pid);
-	printf("Mon PPID : %u\n", my_ppid);
-
+	my_pid = getpid();
+	printf("%u\n", my_pid);
 	return (0);
 }
